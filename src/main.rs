a41d5f8f537ab512850f0f9d5820a07e7ba7028f
Dan Patton <patton.dan@gmail.com>
2025-01-07 23:57:41 +0000

    Faster

--- src/main.rs
+++ src/main.rs
@@ -14,38 +14,33 @@ fn answer(n: usize) -> u128 {
     }
 
     let coins: [usize; 7] = [1, 2, 6, 12, 24, 48, 60];
-    let mut array = [0u128; 28 * M];
+    let mut arr = [0u128; 28 * M];
     for k in (0..M).step_by(2) {
-        array[k] = 1;
+        arr[k] = 1;
     }
 
     for s in (0..n+1).step_by(M) {
-        for i in 1..coins.len() {
+        for i in 1..7 {
             let icoin = coins[i];
-            for j in 0..i + 1 {
+            for k in s..M+s {
+                let z = icoin + 1;
+                arr[_idx(i, 0, k)] = arr[_idx(i-1, 0, k)] + arr[_idx(i, 0, M+k-z)];
+            }
+            for j in 1..i + 1 {
                 let jcoin = coins[j];
                 let z = icoin + jcoin;
-                for _k in 0..M {
-                    let k = s + _k;
-                    let mut q: u128;
-                    if j == 0 {
-                        q = array[_idx(i-1, j, k)];
-                    } else {
-                        let left = array[_idx(i, j-1, k)];
-                        let top = array[_idx(i-1, j, k)];
-                        let diag = array[_idx(i-1, j-1, k)];
-                        q = left + top - diag;
-                    }
-                    if z <= k {
-                        q += array[_idx(i, j, k-z)];
-                    }
-                    array[_idx(i, j, k)] = q;
+                for k in s..M+s {
+                    let left = arr[_idx(i, j-1, k)];
+                    let top = arr[_idx(i-1, j, k)];
+                    let diag = arr[_idx(i-1, j-1, k)];
+                    let back = arr[_idx(i, j, M+k-z)];
+                    arr[_idx(i, j, k)] = left + top - diag + back;
                 }
             }
-        }    
+        }
     }
 
-    array[_idx(6, 6, n)]
+    arr[_idx(6, 6, n)]
 }
 
 fn main() {

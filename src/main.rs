3a1bc5e1e6385f77c0d4fe11371874476c7d64ca
Dan Patton <patton.dan@gmail.com>
2025-01-08 08:50:40 +0000

    Format

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,11 @@ type U192 = BUint<3>;
 const M: usize = 128;
 static S: [usize; 7] = [0, 1, 3, 6, 10, 15, 21];
 
-
 fn answer<T: Num + Copy>(n: usize) -> T {
     #[inline(always)]
     fn _idx(i: usize, j: usize, k: usize) -> usize {
         let [x, y] = cmp::minmax(i, j);
-        (S[y] + x) * M + (k & M-1)
+        (S[y] + x) * M + (k & M - 1)
     }
 
     let coins: [usize; 7] = [1, 2, 6, 12, 24, 48, 60];
@@ -26,21 +25,21 @@ fn answer<T: Num + Copy>(n: usize) -> T {
         arr[k] = T::one();
     }
 
-    for s in (0..n+1).step_by(M) {
+    for s in (0..n + 1).step_by(M) {
         for i in 1..7 {
             let icoin = coins[i];
-            for k in s..M+s {
+            for k in s..M + s {
                 let z = icoin + 1;
-                arr[_idx(i, 0, k)] = arr[_idx(i-1, 0, k)] + arr[_idx(i, 0, M+k-z)];
+                arr[_idx(i, 0, k)] = arr[_idx(i - 1, 0, k)] + arr[_idx(i, 0, M + k - z)];
             }
             for j in 1..i + 1 {
                 let jcoin = coins[j];
                 let z = icoin + jcoin;
-                for k in s..M+s {
-                    let left = arr[_idx(i, j-1, k)];
-                    let top = arr[_idx(i-1, j, k)];
-                    let diag = arr[_idx(i-1, j-1, k)];
-                    let back = arr[_idx(i, j, M+k-z)];
+                for k in s..M + s {
+                    let left = arr[_idx(i, j - 1, k)];
+                    let top = arr[_idx(i - 1, j, k)];
+                    let diag = arr[_idx(i - 1, j - 1, k)];
+                    let back = arr[_idx(i, j, M + k - z)];
                     arr[_idx(i, j, k)] = left + top - diag + back;
                 }
             }
